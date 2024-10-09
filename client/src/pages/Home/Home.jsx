2b4094320c0883218432b246df35ec93271f6577
Raju Kumar <rajukumar150198@gmail.com>
2024-10-09 08:08:20 +0530

    add components

---
+++ client/src/pages/Home/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Slider from '../../components/Slider/Slider';
+import FeaturedProducts from '../../components/FeaturedProducts/FeaturedProducts'
+import "./Home.scss"
+import Categories from '../../components/Categories/Categories';
+const Home = () => {
+  return (
+    <div className='home'>
+      <Slider/>
+      <FeaturedProducts type="featured"/>
+      <Categories/>
+      <FeaturedProducts type="trending"/>
+    </div>
+  )
+}
+
+export default Home
